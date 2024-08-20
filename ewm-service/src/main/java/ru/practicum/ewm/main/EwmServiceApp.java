e10dccb7b08b1939e9d8471ea5369d63ffe84da7
MambaDev <mamba@mail.ru>
2024-08-20 21:46:41 +0300

    fix:  fix fix fix

--- ewm-service/src/main/java/ru/practicum/ewm/main/EwmServiceApp.java
+++ ewm-service/src/main/java/ru/practicum/ewm/main/EwmServiceApp.java
@@ -2,12 +2,8 @@ package ru.practicum.ewm.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {
-        "ru.practicum.ewm.main",
-        "ru.practicum.ewm.client.stats"})
 public class EwmServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(EwmServiceApp.class, args);
--- stats/stats-server/src/main/java/ru/practicum/ewm/server/stats/StatServiceApp.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/server/stats/StatServiceApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class StatServiceApp {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(StatServiceApp.class, args);
     }
 }

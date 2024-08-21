dc4232976f4ee4d0a94632533da683e1e3fdaedb
MambaDev <mamba@mail.ru>
2024-08-21 15:15:01 +0300

    fix: docker-compose and StatMapper

--- stats/stats-server/src/main/java/ru/practicum/ewm/server/stats/service/StatMapper.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/server/stats/service/StatMapper.java
@@ -1,6 +1,7 @@
 package ru.practicum.ewm.server.stats.service;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.practicum.ewm.dto.stats.StatRequestDto;
 import ru.practicum.ewm.dto.stats.StatResponseDto;
 import ru.practicum.ewm.server.stats.model.EndpointHit;
@@ -8,6 +9,8 @@ import ru.practicum.ewm.server.stats.model.ShortEndpointHit;
 
 @Mapper(componentModel = "spring")
 public interface StatMapper {
+
+    @Mapping(target = "id", ignore = true)
     EndpointHit statRequestDtoToEndpointHitData(StatRequestDto statRequestDto);
 
     StatResponseDto shortEndpointHitToStatResponseDto(ShortEndpointHit shortEndpointHit, Integer hits);
